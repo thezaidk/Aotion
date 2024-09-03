f387b7cbcf1f4a86ca702d3547cfcd13f69a76e3
Zaid Karigar <zkarigar1@gmail.com>
2024-09-03 12:56:03 +0530

    fix typo in Logo.tsx component

--- app/(marketing)/_components/logo.tsx
+++ app/(marketing)/_components/logo.tsx
@@ -13,7 +13,7 @@ export const Logo = () => {
             <Image src="/logo.svg" height="40" width="40" alt="Logo" className="dark:hidden" />
             <Image src="/logo-dark.svg" height="40" width="40" alt="Logo" className="hidden dark:block" />
             <p className={cn("font-semibold", font.className)}>
-                Jotion
+                Aotion
             </p>
         </div>
     )
